e6477873f8077a30315a3e9b214922951342f266
App Academy Student <aa@Apps-Mac-mini-3.local>
2019-04-18 17:26:07 -0400

    fin

--- anagrams.rb
+++ anagrams.rb
@@ -17,4 +17,49 @@ def second_anagram?(string_1, string_2)
     string_2.empty?
 end
 
-# 0(n)   Linear Time       MUCH FASTER 
+# 0(n**2)   Quadratic       MUCH FASTER (than factorial)
+
+
+def third_anagram?(string_1, string_2)
+
+    string_1.chars.sort.join == string_2.chars.sort.join
+
+end
+
+#0(n * log(n)) linearithmic time
+
+def fourth_anagram?(string_1, string_2)
+    return false unless string_1.length == string_2.length
+    str1_hash = Hash.new(0)
+    str2_hash = Hash.new(0)
+    string_1.each_char do |char|
+        str1_hash[char] += 1
+    end
+    string_2.each_char do |char|
+        str2_hash[char] += 1
+    end
+    # str1_hash.sort_by{|k, _| k} == str2_hash.sort_by{|k, _| k}
+
+    str1_hash.each_key do |k|
+        return false unless str1_hash[k] == str2_hash[k]
+    end
+    true
+end
+
+#O(n) Linear Time
+
+def bonus_anagram?(string_1, string_2)
+    hash = Hash.new
+    (0...string_1.length).each do |idx|
+        hash[string_1[idx]] = string_2[idx]
+    end
+
+    hash.sort_by {|k, _| k } == hash.sort_by {|_, v| v}
+    # string_1.each_char do |char_1|
+    #     string_2.each_char do |char_2|
+
+    #     end
+    # end
+
+end
+
